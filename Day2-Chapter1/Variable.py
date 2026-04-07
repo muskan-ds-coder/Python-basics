dfc588d1eec763698890a6799a9f1171e04faf0c
muskan-ds-coder <muskan4545783@gmail.com>
2026-04-07 15:04:52 +0530

    practice

---
+++ Day2-Chapter1/Variable.py
@@ -0,0 +1,23 @@
+# A Variable is a container to store data
+x = 12
+name = "Muskan"
+
+# Uses of Variable (Store data)
+age = 21
+
+# Perform calculations
+a = 4
+b = 2
+print(a+b)
+
+#Store user input
+name = input("Enter name: ")
+
+# Reuse values
+price = 50
+total = price * 2
+print(total)
+
+# Many values to Multiple variable
+a, b, c, = 1, 2, 3
+print(a, b, c)
