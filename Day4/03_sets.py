d03e94cabac6021042c8aabaee037392f20caadc
muskan-ds-coder <muskan4545783@gmail.com>
2026-04-11 22:45:28 +0530

    practice

---
+++ Day4/03_sets.py
@@ -0,0 +1,15 @@
+# Sets in Python
+
+'''
+# A set is a collection of unique(no duplicates), unordered, unchangeable*, and unindexed elements.
+# Sets are written with curly brackets {}.
+# A set can contain different types of data, but it cannot contain mutable data types like lists or dictionaries.
+# Sets are one of 4 built-in data types in Python used to store collections of data
+
+'''
+numbers = {1, 2, 3, 4, 5}
+print(numbers)
+
+# Duplicates 
+my_set = {1, 2, 2, 3, 4, 4, 5}
+print(my_set)  # Output: {1, 2, 3, 4, 5}
